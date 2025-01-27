0ac11f968253b5e18056df9687f8402783f2d720
Torch <mistermiland@gmail.com>
2025-01-27 16:23:28 -0700

    added online and offline status to users route

--- internal/routes/users.go
+++ internal/routes/users.go
@@ -9,17 +9,31 @@ type userStripped struct {
 	ID        uint64 `json:"id"`
 	PublicKey string `json:"public_key"`
 	Name      string `json:"name"`
+	Status    status `json:"status"`
 }
+
+type status string
+
+const (
+	Online status = "online"
+	// Idle    status = "idle"
+	// DnD     status = "dnd"
+	Offline status = "offline"
+)
 
 func (s *Service) users(writer http.ResponseWriter, request *http.Request) {
 	writer.Header().Set("Content-Type", "application/json")
 	var users []userStripped
 	for _, user := range s.guild.GetUsers() {
-		// TODO: if id exists in connections, set property online
+		userStatus := Offline
+		if _, err := s.guild.GetUser(user.ID); err == nil {
+			userStatus = Online
+		}
 		users = append(users, userStripped{
 			ID:        user.ID,
 			PublicKey: user.PublicKey,
 			Name:      user.Name,
+			Status:    userStatus,
 		})
 	}
 	if len(users) == 0 {
